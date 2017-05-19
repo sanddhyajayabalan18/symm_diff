cee83dd5aa070349f4952a423593720a0fe8b982
Sanddhya Jayabalan <sanddhyajayabalan18@sasaustin.org>
2017-05-18 20:16:45 -0500

    Added second swap

--- symm_diff_sanddhya.py
+++ symm_diff_sanddhya.py
@@ -1,3 +1,12 @@
+list1 = [2,3,1,4]
+index = 0
+for i in [0,1,2]:
+	if list1[i] > list1[i+1]:
+		t = list1[i+1]
+		list1[i+1] = list1[i]
+		list1[i] = t
+	else:
+		print("Swap not needed")
 index = 0
 for i in [0,1,2]:
 	if list1[i] > list1[i+1]:
